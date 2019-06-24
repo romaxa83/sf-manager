afbfe3b73daf1982d68cb97514872e5ee046bccd
roman <romaxa83@ukr.net>
2019-06-24 22:03:49 +0300

    added members and group for members

---
+++ app/src/Model/Work/Entity/Members/Group/Id.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roomaxa
+ * Date: 24.06.19
+ * Time: 20:28
+ */
+
+namespace App\Model\Work\Entity\Members\Group;
+
+
+class Id
+{
+
+}
---
+++ app/src/Model/Work/UseCase/Members/Group/Edit/Command.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roomaxa
+ * Date: 24.06.19
+ * Time: 20:43
+ */
+
+namespace App\Model\Work\UseCase\Members\Group\Edit;
+
+
+class Command
+{
+
+}
