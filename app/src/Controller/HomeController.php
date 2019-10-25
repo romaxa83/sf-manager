443efdb3daa221cfa7dfc9a64824c0801eab4f3d
roman <romaxa83@ukr.net>
2019-10-25 13:40:52 +0300

    own annotation

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Annotation\RequiresUserCredits;
 use phpcent\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @RequiresUserCredits()
+ */
 class HomeController extends AbstractController
 {
     /**
